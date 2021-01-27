083c80a06e911eef8f878accfb421230a2299dc3
Dylan <yangkd548@163.com>
2021-01-27 21:57:31 +0800

    增加框架启动脚本，方便项目一行代码，启用框架的所有功能 临时增加多个相同“设计模式”类，测试打印的正确性

---
+++ oop/ImportFrame.lua
@@ -0,0 +1,11 @@
+--[[
+    module : ImportFrame
+    author : DylanYang
+    time : 2021-1-27 21:50:46
+    purpose : Import the required framework scripts
+]]
+require("oop.BaseExtend")
+require("oop.TableExtend")
+require("oop.StringExtend")
+require("oop.Class")
+Limit_G()
