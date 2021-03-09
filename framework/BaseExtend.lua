68cb8569ab731f3b7994235675287b0d09075980
yangkd548 <yangkd548@163.com>
2021-03-09 18:09:30 +0800

    调整部分扩展方法的存放文件

--- framework/BaseExtend.lua
+++ framework/BaseExtend.lua
@@ -21,3 +21,88 @@ end
 function IsNumber(v)
     return type(v) == DataTypes.Num
 end
+
+local function printRequire(path, n, a)
+    local str = " "
+    for i = 1, n do
+        str = str .. "\t"
+    end
+    str = n..str..string.format("require : %s:", a)
+    print(str, path)
+end
+
+local tCount = 0
+local isRunBusiness = false
+--Whether to print the require structure of the framework
+local tempPrint = false
+local rawRequire = require
+local function newRrequire(...)
+    local args = {...}
+    local path = args[1]
+    if isRunBusiness or not tempPrint then
+        rawRequire(path)
+    else
+        printRequire(path, tCount, ">>>")
+        tCount = tCount + 1
+        rawRequire(path)
+        tCount = tCount - 1
+        printRequire(path, tCount, "<<<")
+        if tCount == 0 then print("") end
+    end
+    return package.loaded[path]
+end
+
+--The following code is used to solve the Lua language cyclic dependency problem.
+function require(path)
+    if isRunBusiness then
+        return setmetatable({__type = INNER_MT_TYPES.module}, {
+            __index = function(t, k)
+                t = newRrequire(path)
+                if k == "shell" then
+                    return t
+                end
+                return t[k]
+            end
+        })
+    else
+        return newRrequire(path)
+    end
+end
+
+--Avoid overwriting global variables by disabed the definition of global variables.
+function Limit_G()
+    isRunBusiness = true
+    local __g = _G
+    lgv = {}
+    setmetatable(lgv, {
+        __newindex = function(_, name, value)
+            rawset(__g, name, value)
+        end,
+        __index = function(_, name)
+            return rawget(__g, name)
+        end
+    })
+    setmetatable(__g, {
+        __newindex = function(_, name, value)
+            error(string.format("USE \" lgv.%s = value \" INSTEAD OF SET GLOBAL VARIABLE", name), 0)
+        end
+    })
+end
+
+function handler(obj, method)
+    return function(...)
+        return method(obj, ...)
+    end
+end
+
+function handlerPlus(obj, method, ...)
+    local args = {...}
+    return function(...)
+        return method(obj, unpack(args), ...)
+    end
+end
+
+luaVersion = _VERSION
+
+--Unify the differences in the 'loadstring' function in the different versions of Lua.
+loadstring = loadstring or load
